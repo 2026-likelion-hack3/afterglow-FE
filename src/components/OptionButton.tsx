3d02f5b45aef5659c2998ff728d46b55bd9aca24
nyeohijyul <nyeohijyul@gmail.com>
2026-08-13 16:30:37 +0900

    feat: add Option button component

---
+++ src/components/OptionButton.tsx
@@ -0,0 +1,38 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Colors } from "../constants/colors";
+import { Typography } from "../constants/typography";
+
+type OptionButtonProps = {
+    text: string,
+    onPress: () => void,
+    isSelected: boolean
+}
+
+const Styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: Colors.border.default,
+        borderRadius: 16,
+        paddingHorizontal: 23,
+        backgroundColor: Colors.background.card,
+    },
+    selected: {
+        borderColor: Colors.accent.dark,
+        backgroundColor: Colors.accent.default,
+    }
+})
+
+export default function OptionButton({ text, onPress, isSelected }: OptionButtonProps) {
+    return (
+        <Pressable
+            onPress={ onPress }
+            style={ [Styles.button, isSelected && Styles.selected] }
+        >
+            <Text style={ Typography.text.default }>{ text }</Text>
+        </Pressable>
+    )
+}
