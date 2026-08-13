06c100265e384631117f8daa175186ddc620adf6
nyeohijyul <nyeohijyul@gmail.com>
2026-08-13 18:47:04 +0900

    feat: add style presets

---
+++ src/constants/preset.ts
@@ -0,0 +1,14 @@
+export const Presets = {
+    tag: {
+        alignSelf: 'flex-start',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 999,
+        paddingVertical: 4,
+        paddingHorizontal: 8
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+}
