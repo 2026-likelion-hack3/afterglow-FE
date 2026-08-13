0929cb9f74b133603ac11e7bed5f0136c531182e
nyeohijyul <nyeohijyul@gmail.com>
2026-08-13 17:40:54 +0900

    feat: tag component

---
+++ src/components/Tag.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Typography } from "../constants/typography";
+import { Colors } from "../constants/colors";
+
+const Styles = StyleSheet.create({
+    tag: {
+        alignSelf: 'flex-start',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: Colors.alert.text
+    }
+})
+
+type TagProps = {
+    text: string
+}
+
+export default function Tag({ text }: TagProps) {
+    return (
+        <View style={ Styles.tag }>
+            <Text style={ [Typography.label.default, { color: Colors.alert.text }] }>{ text }</Text>
+        </View>
+    )
+}
