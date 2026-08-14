d2842c6c343541fc0e9220e26a23b1f75b986bdd
nyeohijyul <nyeohijyul@gmail.com>
2026-08-14 11:58:45 +0900

    feat: apply font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,11 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font"
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Noto Sans KR": require("../assets/fonts/NotoSansKR.ttf"),
+  });
   return (
     <SafeAreaProvider>
       <Stack screenOptions={{ headerShown: false }} />
