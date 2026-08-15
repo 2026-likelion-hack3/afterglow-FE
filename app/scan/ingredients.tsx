3b6a642206e1f037ea7adcfff13241467dea9d42
nyeohijyul <nyeohijyul@gmail.com>
2026-08-16 01:35:43 +0900

    feat: implement detecting ingredients screen ui

--- app/scan/ingredients.tsx
+++ app/scan/ingredients.tsx
@@ -2,10 +2,6 @@
  * 성분표 촬영 화면
  */
 
-/**
- * 제품 스캔 화면
- */
-
 import ActionButton from "@/src/components/ActionButton";
 import HeaderNavigation from "@/src/components/HeaderNavigation";
 import SecondaryActionButton from "@/src/components/SecondaryActionButton";
@@ -35,13 +31,19 @@ export default function ScanScreen() {
                     <Text style={[Typography.secondary.default, {color: Colors.text.secondary}]}>함께 쓰면 안 되는 조합을 알려드릴 수 있어요</Text>
                 </View>
                 {/* 개발 필요 */}
-                <View style={{height: 230, borderStyle: 'dashed', borderWidth: 1, borderColor: Colors.sand[400], backgroundColor: Colors.background.subtle}}>
+                <View style={{borderStyle: 'dashed', borderWidth: 1, borderColor: Colors.sand[400], backgroundColor: Colors.background.subtle}}>
                     <Text>카메라화면미개발</Text>
                 </View>
-                <View style={{ gap: 8, borderStyle: 'solid', borderWidth: 1, borderColor: Colors.border.default, borderRadius: 16, backgroundColor: Colors.background.card }}>
-                    <Text style={[Typography.text.accent]}>글씨가 안 읽히나요?</Text>
-                    <Text style={[Typography.secondary.small, {color: Colors.text.secondary}]}>밝은 곳에서 다시 찍거나, 아래에서 이름을 직접 적으셔도 됩니다.</Text>
+                <View style={{gap: 10, borderStyle: 'solid', borderWidth: 1, borderColor: Colors.border.default, borderRadius: 16, padding: 20, backgroundColor: Colors.background.card }}>
+                    <Text style={[Typography.label.default, {color:Colors.text.secondary}]}>이런 걸 찾아냅니다</Text>
+                    {['레티놀', '산', '비타민씨'].map((text)=>(
+                        <View style={[{alignSelf:'flex-start', borderWidth: 1, borderStyle: 'solid', borderColor: Colors.border.default, borderRadius: 200, paddingVertical: 8, paddingHorizontal:16 }]}>
+                            <Text style={[Typography.label.default, {color: Colors.text.secondary}]}>{text}</Text>
+                        </View>
+                    ))}
+                    <Text style={[Typography.secondary.small, {color: Colors.text.muted}]}>성분 전체를 저장하지 않고 이 태그만 남깁니다</Text>
                 </View>
+                <Text style={[Typography.secondary.small, {color: Colors.text.secondary}]}>건너뛰셔도 등록은 끝납니다. 나중에 추가하셔도 돼요.</Text>
             </View>
             </ScrollView>
 
